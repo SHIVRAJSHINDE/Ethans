6a609cd7a9ad9a2c7d4a52294e5e5d6b829f4286
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2026-09-25 13:18:04 +0530

    --

---
+++ Batch-26/Python/01 Basic Python/01_DataTypes_&_Variables_Operatores/test.py
@@ -0,0 +1,11 @@
+# Sequence
+x = {1,5.5,"Raj",True}
+#    0,1  ,2     ,3
+
+print(x[2])
+
+# List            Tuple            # Sets
+# mutalbe         Imuttalbe        mutable
+# Support Index   Support Index    Do not Support Index 
+# Hold Duplicate  Hold Duplicate   Do Not Hold Duplicate 
+
